39b44ecb0202c8d79015575dd620ded09f2711e1
Adrian Paliwoda <adrian94@tutanota.com>
2024-12-23 14:07:34 +0100

    Added more staled-component elements

--- 06-starting-project/src/components/AuthInputs.jsx
+++ 06-starting-project/src/components/AuthInputs.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import {styled} from 'styled-components';
+import { useState } from "react";
+import { styled } from "styled-components";
 
 const ControlContainer = styled.div`
   display: flex;
@@ -8,13 +8,34 @@ const ControlContainer = styled.div`
   margin-bottom: 1.5rem;
 `;
 
+const Label = styled.label`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({invalid}) => invalid ? '#f87171' : '#6b7280'};
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: ${(invalid) => invalid ? '#fed2d2' : '#d1d5db'};;
+  color: ${(invalid) => invalid ? '#ef4444' : '#374151'};
+  border: 1px solid ${({invalid}) => invalid ? '#f73f3f' : 'transparent'};
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -25,28 +46,29 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
     <div id="auth-inputs">
       <ControlContainer>
         <p>
-          <label className={`label ${emailNotValid ? 'invalid' : undefined}`} >Email</label>
-          <input
-          // style={{backgroundColor: emailNotValid ? 'red' : "#d1d6db"}}
+          <Label>Email</Label>
+          <Input
+            // style={{backgroundColor: emailNotValid ? 'red' : "#d1d6db"}}
+            invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            className={emailNotValid ? "invalid" : undefined}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
-          <input
+          <Label>Password</Label>
+          <Input
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            invalid={passwordNotValid}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
@@ -55,7 +77,9 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className="button" onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
