0bc005cc0246b4580e19fb636cb0853748af3bf8
Adrian Paliwoda <adrian94@tutanota.com>
2025-02-08 20:36:27 +0100

    Added error handling

--- 15-starting-project/src/components/AvailablePlaces.jsx
+++ 15-starting-project/src/components/AvailablePlaces.jsx
@@ -1,27 +1,53 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import ErrorPage from "./Error.jsx";
 
 const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    const places = fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((results) => {
-        setAvailablePlaces(results.places);
-        localStorage.setItem("places", JSON.parse(results.places));
-      })
-      .catch(() => console.log("Cannot fetch places"));
+    async function fetchPlaces() {
+      setIsLoading(true);
+
+      try {
+        // const response = await fetch("http://localhost:3000/places2");
+        const response = await fetch("http://localhost:3000/places");
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          const error = new Error("Failed to fetch places");
+          throw error;
+        }
+
+        setAvailablePlaces(responseData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later",
+        });
+        console.log("Cannot fetch places!");
+      }
+
+      setIsLoading(false);
+    }
+
+    fetchPlaces();
   }, []);
+
+  if (error) {
+    return <ErrorPage title="An error occured!" message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoading}
+      loadingPlaces="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
