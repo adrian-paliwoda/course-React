778959c121ea2079a21fb1076d665dc07cc30607
Adrian Paliwoda <adrian94@tutanota.com>
2025-02-17 20:45:15 +0100

    Added validation to Login page

--- 17-starting-project/src/components/Login.jsx
+++ 17-starting-project/src/components/Login.jsx
@@ -1,23 +1,83 @@
+import { useRef, useState } from "react";
+
 export default function Login() {
+  const email = useRef("");
+  const password = useRef("");
+
+  const [emailIsValid, setEmailIsValid] = useState(true);
+  const [passwordIsValid, setPasswordIsValid] = useState(true);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submited");
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    console.log(enteredEmail);
+    console.log(enteredPassword);
+    // event.target.reset();
+  }
+
+  function handleReset(event) {
+    event.target.reset();
+  }
+
+  function handleEmailBlur() {
+    const enteredEmail = email.current.value;
+
+    if (enteredEmail.includes("@")) {
+      setEmailIsValid(true);
+    } else {
+      setEmailIsValid(false);
+    }
+  }
+
+  function handlePasswordBlur() {
+    const enteredPassword = password.current.value;
+
+    if (enteredPassword) {
+      setPasswordIsValid(true);
+    } else {
+      setPasswordIsValid(false);
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onBlur={handleEmailBlur}
+            ref={email}
+          />
+        </div>
+        <div className="control-error">
+          {!emailIsValid && <p>Please enter a valid email</p>}
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onBlur={handlePasswordBlur} ref={password} />
         </div>
       </div>
+      <div className="control-error">
+          {!passwordIsValid && <p>Please enter a valid password</p>}
+        </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button button-flat" onClick={handleReset}>
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Login
+        </button>
       </p>
     </form>
   );
