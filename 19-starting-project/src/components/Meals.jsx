1aca21af30e6ca52438d0eeeffbfbf32bbeb5885
Adrian Paliwoda <adrian94@tutanota.com>
2025-04-14 15:21:51 +0200

    Added http hook

--- 19-starting-project/src/components/Meals.jsx
+++ 19-starting-project/src/components/Meals.jsx
@@ -1,25 +1,24 @@
-import { useFetch } from "../hooks/useFetch";
-import { fetchMeals } from "../utils/gateway";
+import useHttp from "../hooks/useHttp.js";
 import Error from "./Error.jsx";
 import MealItem from "./MealItem.jsx";
+
+const requestConfig = {};
 
 export default function Meals({
   loadingText = "Fetching your meals...",
   fallbackText = "No meals avaiable.",
 }) {
   const {
-    isFetching,
+    data: meals,
     error,
-    fetchedData: meals,
-    setFetchedData: setMeals,
-  } = useFetch(fetchMeals, []);
-
+    loading: isFetching,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
   return (
     <>
       {error && <Error title="An error occurred!" message={error.message} />}
-      {isFetching && <p className="fallback-text">{loadingText}</p>}
+      {isFetching && <p className="fallback-text center">{loadingText}</p>}
       {!isFetching && meals.length === 0 && (
-        <p className="fallback-text">{fallbackText}</p>
+        <p className="fallback-text center">{fallbackText}</p>
       )}
       {!error && !isFetching && meals.length > 0 && (
         <ul id="meals">
--- 19-starting-project/src/store/CartContext.jsx
+++ 19-starting-project/src/store/CartContext.jsx
@@ -1,18 +1,20 @@
-import { createContext, useReducer} from "react";
+import { createContext, useReducer } from 'react';
 
 const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
-  if (action.type === "ADD_ITEM") {
+  if (action.type === 'ADD_ITEM') {
     const existingCartItemIndex = state.items.findIndex(
-      (p) => p.id === action.item.id
+      (item) => item.id === action.item.id
     );
 
     const updatedItems = [...state.items];
+
     if (existingCartItemIndex > -1) {
       const existingItem = state.items[existingCartItemIndex];
       const updatedItem = {
@@ -27,26 +29,29 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
-  if (action.type === "REMOVE_ITEM") {
+  if (action.type === 'REMOVE_ITEM') {
     const existingCartItemIndex = state.items.findIndex(
-      (p) => p.id === action.id
+      (item) => item.id === action.id
     );
+    const existingCartItem = state.items[existingCartItemIndex];
 
-    const existingItem = state.items[existingCartItemIndex];
     const updatedItems = [...state.items];
 
-    if (existingItem.quantity === 1) {
+    if (existingCartItem.quantity === 1) {
       updatedItems.splice(existingCartItemIndex, 1);
     } else {
       const updatedItem = {
-        ...existingItem,
-        quantity: existingItem.quantity - 1,
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
       };
-
       updatedItems[existingCartItemIndex] = updatedItem;
     }
 
     return { ...state, items: updatedItems };
+  }
+
+  if (action.type === 'CLEAR_CART') {
+    return { ...state, items: [] };
   }
 
   return state;
@@ -56,20 +61,27 @@ export function CartContextProvider({ children }) {
   const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
   function addItem(item) {
-    dispatchCartAction({type: 'ADD_ITEM', item})
+    dispatchCartAction({ type: 'ADD_ITEM', item });
   }
 
   function removeItem(id) {
-    dispatchCartAction({type: 'REMOVE_ITEM', id})
+    dispatchCartAction({ type: 'REMOVE_ITEM', id });
+  }
+
+  function clearCart() {
+    dispatchCartAction({ type: 'CLEAR_CART' });
   }
 
   const cartContext = {
     items: cart.items,
     addItem,
-    removeItem
-  }
+    removeItem,
+    clearCart
+  };
 
-  return <CartContext value={cartContext}>{children}</CartContext>;
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
 }
 
 export default CartContext;
