c673e0d83e4dd6ea43e971d7892bc1347629bbdd
Adrian Paliwoda <adrian94@tutanota.com>
2025-05-03 12:14:01 +0200

    Used cart store

--- 21-starting-project/src/store/cart-slice.js
+++ 21-starting-project/src/store/cart-slice.js
@@ -9,9 +9,10 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
-      if (existingItem) {
+      state.totalQuantity++;
+      if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -22,7 +23,17 @@ const cartSlice = createSlice({
         existingItem.totalPrice += newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else if (existingItem.quantity > 1) {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
 
