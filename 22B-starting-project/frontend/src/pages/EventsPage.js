bb966cb72f59ad735be3df097fb154a12ef8448c
Adrian Paliwoda <adrian94@tutanota.com>
2025-07-17 12:35:55 +0200

    Separated load events loader

--- 22B-starting-project/frontend/src/pages/EventsPage.js
+++ 22B-starting-project/frontend/src/pages/EventsPage.js
@@ -1,9 +1,20 @@
-import { useLoaderData } from 'react-router-dom';
-import EventsList from '../components/EventsList';
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData()
-  return <EventsList events={events}/>;
+  const events = useLoaderData();
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    return undefined;
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
